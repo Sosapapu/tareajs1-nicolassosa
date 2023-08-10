d3b81bb7862c150821f1f75925adb4a0c9738e1f
Sosa Nico <140737842+Sosapapu@users.noreply.github.com>
2023-08-10 12:09:46 -0300

    Add files via upload

---
+++ nashe.js
@@ -0,0 +1,21 @@
+
+
+function sumarNumeros() {
+      var num1 = parseInt(document.getElementById("num1").value);
+      var num2 = parseInt(document.getElementById("num2").value);
+      var suma = num1 + num2;
+      alert(suma);
+    }
+
+    function restarNumeros() {
+        var num1 = parseInt(document.getElementById("num1").value);
+        var num2 = parseInt(document.getElementById("num2").value);
+        var resta = num1 - num2;
+        alert(resta);
+      }
+      function multiplicarNumeros() {
+        var num1 = parseInt(document.getElementById("num1").value);
+        var num2 = parseInt(document.getElementById("num2").value);
+        var multiplicacion = num1 * num2;
+        alert(multiplicacion);
+      }
